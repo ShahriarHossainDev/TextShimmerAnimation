102e0a392f6020dfbb0fb61bc3b5efb7f4b85f42
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2024-10-12 13:55:32 +0600

    Initial Commit

---
+++ TextShimmerAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TextShimmerAnimation
+//
+//  Created by Shishir_Mac on 12/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TextShimmerAnimation/TextShimmerAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextShimmerAnimationApp.swift
+//  TextShimmerAnimation
+//
+//  Created by Shishir_Mac on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TextShimmerAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
